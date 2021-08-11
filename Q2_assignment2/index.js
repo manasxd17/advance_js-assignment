ea729c7dfd40fddca50faa7cbc631203a1c4118c
Manas Maheswari <manas.maheshwari26@nmims.edu.in>
2021-08-12 00:51:21 +0530

    init

--- Q2_assignment2/index.js
+++ Q2_assignment2/index.js
@@ -7,21 +7,25 @@ const QueenAttak = class{
     canAttack(){
         let [x1,y1] = this.p1;
         let [x2,y2] = this.p2;
-
-        if(x1 == y1){
-            console.log("Can attak")
+        if(0 <= x1,x2,y1,y2 <= 7){
+            if(x1 == y1){
+                console.log("Can attak")
+            }
+    
+            else if(x2 == y2){
+                console.log("Can attack")
+            }
+    
+            else if(Math.abs(x1 - y1) == Math.abs(x2 - y2)){
+                console.log("Can attack")
+            }
+    
+            else{
+                console.log("Cannot Attack")
+            }
         }
-
-        else if(x2 == y2){
-            console.log("Can attack")
-        }
-
-        else if(Math.abs(x1 - y1) == Math.abs(x2 - y2)){
-            console.log("Can attack")
-        }
-
         else{
-            console.log("Cannot Attack")
+            console.log("Check the points")
         }
     }
 }
